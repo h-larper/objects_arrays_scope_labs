eb7c0f34d9474b2f711aedb37f7e2c2a108f7772
Laura Harper <lauraharper95@gmail.com>
2024-03-11 15:59:12 +0000

    added and instantiated a new parrot object

--- objects_arrays_lab/code/objects.js
+++ objects_arrays_lab/code/objects.js
@@ -54,10 +54,15 @@ console.log("The dog has " + dogColourCount + " different colours");
 /**
  * Q5. I've just bought a 12-year-old African Grey parrot called Lucky. Create an object for her.
  */
+parrot = {
+  name: "Lucky",
+  age: 12,
+  breed: "African Grey",
+  colours: ["Grey", "Red"],
+};
 
-parrot = null;
+console.log(parrot);
 
-// console.log(parrot);
 
 /**
  * Q6. Ella's favourite food is dandelion, add this information to her object
